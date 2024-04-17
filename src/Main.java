ad136a82b8f59adbdd920915f7ea4d9cc4ab5fbe
Eugen <eugen283455@gmail.com>
2024-04-17 19:02:27 +0300

    print version

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		Greeting greetting = new Greeting("Hello, User.\nIt was a greeting...");
 		System.out.println(greetting.getGreet());
-	}
+		System.out.println("version 2 test.");	}
 
 }
